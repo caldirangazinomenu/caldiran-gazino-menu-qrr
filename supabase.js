a5335a87995441ef1be70bd710bd15d578b8ceb0
caldirangazinomenu <1.hd.tb.askerigazino@gmail.com>
2026-07-17 10:07:55 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://caldiran-gazino-menu-qrr.vercel.app/admin/admin.html";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6Y3RneWxtbGZ6a3B6bWFiZHVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDMwMjEsImV4cCI6MjA5ODExOTAyMX0.X0u47jcQK5AEg8Poa9ZdgXnbkV4xVQ2j0LMgn5-DprE";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdHV6YnhuYnFoZG5tcmJ2cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MjkyNjksImV4cCI6MjA5OTAwNTI2OX0.-9wpNXoG1g5YCYEbUgJ63C2vpNfrbg8n6W6gaAgpM4w";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
