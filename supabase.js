003629469d27d50e1d3b74c536756bcf823a26aa
caldirangazinomenu <1.hd.tb.askerigazino@gmail.com>
2026-07-22 13:05:43 +0300

    Update Supabase URL and retain API key

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "sb_publishable_jFpQ6EAn8KLDwNgf_2IRrg_jpbeSMFq";
+const SUPABASE_URL = "https://gctuzbxnbqhdnmrbvrjm.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdHV6YnhuYnFoZG5tcmJ2cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MjkyNjksImV4cCI6MjA5OTAwNTI2OX0.-9wpNXoG1g5YCYEbUgJ63C2vpNfrbg8n6W6gaAgpM4w";
 
