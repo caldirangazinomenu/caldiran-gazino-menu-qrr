93b2428ed3c3e2e7ccb55a0aa38270d9641c893d
caldirangazinomenu <1.hd.tb.askerigazino@gmail.com>
2026-07-22 13:11:46 +0300

    Update Supabase URL and key initialization

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://gctuzbxnbqhdnmrbvrjm.supabase.co/rest/v1/caldirangazinomenu";
+const SUPABASE_URL = "https://gctuzbxnbqhdnmrbvrjm.supabase.co/rest/v1/";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdHV6YnhuYnFoZG5tcmJ2cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MjkyNjksImV4cCI6MjA5OTAwNTI2OX0.-9wpNXoG1g5YCYEbUgJ63C2vpNfrbg8n6W6gaAgpM4w";
 
